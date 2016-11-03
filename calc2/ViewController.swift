5b23def9fc186ced8358ef33d37daea3b2789498
lucas persson <luben93@gmail.com>
2016-11-03 20:58:16 +0100

    frl1

--- calc2/ViewController.swift
+++ calc2/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            print("touch: \(digit)")
+            display.text?.append(digit)
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathSymbol = sender.currentTitle{
+            if mathSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
-
 
 }
 
